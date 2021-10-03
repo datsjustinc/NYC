705addbb08a88c12975f61130703dbf692e3cfba
datsjustinc <jc9957@nyu.edu>
2021-10-03 12:15:21 -0400

    Rube Goldberg Machine
    First Commit for Machine

--- Assets/Scripts/MySimpleTrigger.cs
+++ Assets/Scripts/MySimpleTrigger.cs
@@ -2,37 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MySimpleTrigger : MonoBehaviour
-{
+public class MySimpleTrigger : MonoBehaviour {
+
     private BoxCollider2D bc;
 
-    private void Awake()
-    {
+    private void Awake() {
         bc = GetComponent<BoxCollider2D>();
         bc.isTrigger = true;
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) // object enters collision
-    {
-        if (collision.CompareTag("Player"))
-        {
+
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+
+        if (collision.CompareTag("Ball")) {
+            bc.isTrigger = false;
             collision.GetComponent<SpriteRenderer>().color = new Color(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
         }
-        else
-        {
-            collision.gameObject.SetActive(false);
-        }
-
-        Debug.Log("A collision has happened!!", gameObject); // print to console, gameObject shows on console and pings what collided, represents the object that passed through the trigger
+        
+        Debug.Log("A collision has happened!!", gameObject);
     }
 
-     private void OnTriggerExit2D(Collider2D collision) 
-     {
-        Debug.Log("A collision has happened again!!"); // print to console
+    private void OnTriggerExit2D(Collider2D collision) {
+        Debug.Log("A collision has happened again!!!");
     }
 
-    private void OnTriggerStay2D(Collider2D collision)
-    {
+    private void OnTriggerStay2D(Collider2D collision) {
         
     }
 }
