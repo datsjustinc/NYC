f7421b3c088d8389a9d863e05bcedc2a2c453486
datsjustinc <jc9957@nyu.edu>
2021-10-25 18:16:38 -0400

    Update v2.3.2.4
    1. Fixed bug where player score doesn't reset after pressing restart button on first map.

--- Assets/Scripts/Exploration/RestartGame.cs
+++ Assets/Scripts/Exploration/RestartGame.cs
@@ -6,14 +6,22 @@ using UnityEngine.SceneManagement;
 public class RestartGame : MonoBehaviour
 {
     public string restartScene; // create field variable to store scene name
-    // Start is called before the first frame update
+
+    public PlayerScore value; // create value to reference another script
 
     // Update is called once per frame
+
+    void Awake()
+    {
+        value = GameObject.Find("Score").GetComponent<PlayerScore>(); // find object that script is in and get the script
+    }
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.R)) // if key pressed
         {
             SceneManager.LoadScene(restartScene); // load next scene
+            value.reset = true;
+
         }
     }
 }
