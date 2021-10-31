ffd2298ed1b7bec1c9af816b8f7ebe4265ecc6f2
datsjustinc <jc9957@nyu.edu>
2021-10-31 17:15:45 -0400

    Update v2.4.4.5
    1. Fixed bug with player starting with 4 coins in first map rather than in final map.

--- Assets/Scripts/Exploration/PlayerScore.cs
+++ Assets/Scripts/Exploration/PlayerScore.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class PlayerScore : MonoBehaviour 
 {
-
+    Scene currentScene; // create variable to store scene
+    string sceneName; // create variable to store scene name
     public static int points; // create variable that can be accessed outside from another script
     public int pt = points; // create field variable to assign points to
     public TMP_Text displayScore; // create field variable to hold text
@@ -14,10 +16,17 @@ public class PlayerScore : MonoBehaviour
 
     public void Awake() // function called before game starts
     {
+        currentScene = SceneManager.GetActiveScene();
+        sceneName = currentScene.name;
         displayScore = GetComponent<TMP_Text>(); // get text component and store in display score
 
         pt = 0; // set player score to 0
-        points = 4;
+
+        if (sceneName == "Little Island Exploration 2")
+        {
+            points = 4;
+        }
+        
         reset = false;
     }
 
